dacfb853aedb5d74dd72a6c56da8ea5739f11c64
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-03-28 08:34:36 +0100

    ho inserito l' unit test

--- Fattoriale_pacini/CalcoliCombinatori.cs
+++ Fattoriale_pacini/CalcoliCombinatori.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Fattoriale_pacini
+namespace EquazioniLibrary
 {
-    public class Class1
+    public class CalcoliCombinatori
     {
+        
     }
 }
